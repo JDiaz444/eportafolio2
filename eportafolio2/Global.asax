cd797f7bab74d655eae3b29da955c35211c9c5a6
JAaronDV <Astraphobia@NITRO5>
2023-06-01 11:51:04 -0700

    Add project files.

---
+++ eportafolio2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eportafolio2.MvcApplication" Language="C#" %>
---
+++ eportafolio2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eportafolio2.Startup))]
+namespace eportafolio2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
